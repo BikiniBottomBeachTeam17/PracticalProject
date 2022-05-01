0c217785fa4b7f5026b488c9b1cd45f5508055a1
MuStar <673818565@qq.com>
2022-05-01 22:35:05 +0800

    Mapper layer structures

---
+++ 朴朴数据可视化--后端/src/main/java/com/mustar/mapper/DatasMapper.java
@@ -0,0 +1,21 @@
+package com.mustar.mapper;
+
+import com.mustar.entity.Datas;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mustar.entity.Product;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author mustar
+ * @since 2022-04-29
+ */
+public interface DatasMapper extends BaseMapper<Datas> {
+
+}
---
+++ 朴朴数据可视化--后端/src/main/java/com/mustar/mapper/ItemsMapper.java
@@ -0,0 +1,16 @@
+package com.mustar.mapper;
+
+import com.mustar.entity.Items;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author mustar
+ * @since 2022-04-29
+ */
+public interface ItemsMapper extends BaseMapper<Items> {
+
+}
