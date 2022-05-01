3bd9498b0b28e519866a521a60c7e007214bf595
huangjialin <3049552017@qq.com>
2022-05-01 22:41:01 +0800

    According to the commodity UUID query commodity information list mapper layer

--- 朴朴数据可视化--后端/src/main/java/com/mustar/mapper/DatasMapper.java
+++ 朴朴数据可视化--后端/src/main/java/com/mustar/mapper/DatasMapper.java
@@ -28,4 +28,17 @@ public interface DatasMapper extends BaseMapper<Datas> {
             "ON items.uuid=new.uuid\n" +
             "WHERE new.time=datas.querytime")
     List<Product> getProduct();
+    @Select("SELECT items.*,datas.price,datas.querytime\n" +
+            "FROM items LEFT JOIN datas\n" +
+            "ON items.uuid=datas.uuid\n" +
+            "RIGHT JOIN (\n" +
+            "SELECT items.uuid,MAX(datas.querytime) time \n" +
+            "FROM items Left JOIN datas \n" +
+            "ON datas.uuid=items.uuid \n" +
+            "GROUP BY items.uuid) new\n" +
+            "ON items.uuid=new.uuid\n" +
+            "WHERE new.time=datas.querytime\n" +
+            "AND items.uuid='${uuid}'")
+    Product getProductInfo(@Param("uuid")String uuid);
+
 }
