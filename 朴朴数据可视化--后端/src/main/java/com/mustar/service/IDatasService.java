b429b898aa1207aa82221ef545d0350fb55ba77d
huangjialin <3049552017@qq.com>
2022-05-01 22:24:49 +0800

    Creation of service layer classes

---
+++ 朴朴数据可视化--后端/src/main/java/com/mustar/service/IDatasService.java
@@ -0,0 +1,16 @@
+package com.mustar.service;
+
+import com.mustar.entity.Datas;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author mustar
+ * @since 2022-04-29
+ */
+public interface IDatasService extends IService<Datas> {
+
+}
---
+++ 朴朴数据可视化--后端/src/main/java/com/mustar/service/IItemsService.java
@@ -0,0 +1,16 @@
+package com.mustar.service;
+
+import com.mustar.entity.Items;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author mustar
+ * @since 2022-04-29
+ */
+public interface IItemsService extends IService<Items> {
+
+}
