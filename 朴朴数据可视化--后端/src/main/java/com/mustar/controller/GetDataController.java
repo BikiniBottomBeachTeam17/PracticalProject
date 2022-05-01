2746d2158aaed573f4a6cde149a900be17d2883f
MuStar <673818565@qq.com>
2022-05-01 22:52:39 +0800

    Interface development to retrieve data and store it in a database

--- 朴朴数据可视化--后端/src/main/java/com/mustar/controller/GetDataController.java
+++ 朴朴数据可视化--后端/src/main/java/com/mustar/controller/GetDataController.java
@@ -23,5 +23,53 @@ import java.util.List;
 @RestController
 @RequestMapping("/getData")
 public class GetDataController {
+    @Autowired
+    private DatasMapper datasMapper;
+    @Autowired
+    private ItemsMapper itemsMapper;
+    //获取数据并保存到数据库
+    @PostMapping("/saveData")
+    public Result saveData(@RequestBody Product product){
+        //判断数据库中是否存在该商品
+        if (itemsMapper.selectOne(Wrappers.<Items>lambdaQuery().eq(Items::getUuid, product.getUuid()))!=null){
+            //存在查看是否有当天数据
+            if (datasMapper.selectOne(Wrappers.<Datas>lambdaQuery()
+                    .eq(Datas::getUuid, product.getUuid())
+                    .eq(Datas::getQuerytime, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))))!=null){
+                return Result.error(-1,"该商品已有当日数据，不予以更新");
+            }
+            //存在则添加价格表
+            Datas datas=new Datas();
+            datas.setUuid(product.getUuid());
+            datas.setPrice(product.getPrice());
+            datas.setQuerytime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+            if(datasMapper.insert(datas)!=0){
+                return Result.success("该商品已存在数据库中，价格更新成功");
+            }else {
+                return Result.error(-1,"该商品已存在数据库中，价格更新失败");
+            }
+        }else{
+            //不存在则添加商品表并且添加价格表
+            Items items=new Items();
+            items.setUuid(product.getUuid());
+            items.setName(product.getName());
+            items.setSubtitle(product.getSubTitle());
+            items.setOrigin(product.getOrigin());
+            items.setImg(product.getImg());
+            if (itemsMapper.insert(items)!=0){
+                Datas datas=new Datas();
+                datas.setUuid(product.getUuid());
+                datas.setPrice(product.getPrice());
+                datas.setQuerytime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+                if(datasMapper.insert(datas)!=0){
+                    return Result.success("该商品已添加到数据库中，且价格更新成功");
+                }else {
+                    return Result.error(-1,"该商品已添加到数据库中，价格更新失败");
+                }
+            }else {
+                return Result.error(-1, "该商品添加到数据库失败");
+            }
+        }
+    }
    
 }
